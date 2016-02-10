c55eb0b16cd10d7b1e8e868cea5d8fa61faa0990
megaentity <jafari@live.ca>
2016-02-09 19:02:37 -0800

    initial commit

---
+++ helloworld.rb
@@ -0,0 +1,12 @@
+puts "jaaafaaaar"
+
+puts = "what is your first name? "
+first_name = gets.chomp
+
+puts = "what is your last name? "
+last_name = gets.chomp
+
+full_name = puts "your full name is #{first_name} #{last_name}"
+
+
+
